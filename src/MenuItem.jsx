dcff6b67eae3ae1c6df94a9fcb917ee2c0c10828
Antônio <toninhoport@gmail.com>
2023-07-21 01:27:20 -0300

    update: update on projects section

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function MenuItem({title,desc,number}) {
+export default function MenuItem({title,desc,icon,active}) {
   return (
-    <div className='menu-item'>
-      <div>
-       <p> {number}</p>
+    <div style={active ? {backgroundColor:"#EA3C53",transition: "all .14s ease"} : {backgroundColor:"transparent",transition: "all .14s ease"}} className='menu-item' data-aos="fade-right" data-aos-offset="100" data-aos-delay="700">
+      <div className='icon-div'>
+       {icon}
       </div>
-      <div>
+      <div className='desc-menu-item'>
        <h4>{title}</h4>
        <p>{desc}</p>
       </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Project from "./Project";
 import MenuItem from "./MenuItem";
+import { TbHome2 } from "react-icons/tb";
 
 export default function Projects() {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    },{ rootMargin: "-300px" });
+    observer.observe(ref.current);
+    console.log(isIntersecting);
+
+    return () => observer.disconnect();
+
+  },[])
+
   return (
     <>
       <div className="project-container">
@@ -10,22 +27,40 @@ export default function Projects() {
           <h2 data-aos="fade-right" data-aos-offset="200" data-aos-delay="50">
             PROJECTS
           </h2>
-          
+
           <h2 data-aos="fade-right" data-aos-offset="200" data-aos-delay="400">
             -
           </h2>
-         <div className="menu">
-          <div>
-          <MenuItem number={"1 - "} title={"Housing Hub"} desc={"A marketplace for real estate"} />
+          <div className="menu">
+            <div>
+              <section >
+                <MenuItem
+                  active={isIntersecting}
+                  icon={<TbHome2 className="icon" />}
+                  title={"Housing Hub"}
+                  desc={"A marketplace for real estate"}
+                />
+              </section>
+            </div>
           </div>
-          
-         </div>
         </div>
         <div className="bordered-container-project">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
+          <section className="project-section" ref={ref}
+>
+          <Project  />
+          </section>
+          <section className="project-section"  >
+          <Project  />
+          </section>
+          <section className="project-section"  >
+          <Project  />
+          </section>
+          <section className="project-section"  >
+          <Project  />
+          </section>
+          <section className="project-section"  >
+          <Project  />
+          </section>
         </div>
       </div>
     </>
